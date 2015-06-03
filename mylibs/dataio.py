0f8035fa358acb89c877c13e609c50548600014f
yousa <yousa@penguin>
2015-06-03 23:41:09 +0900

    Add dataio.py into project.

---
+++ mylibs/dataio.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import inspect
+import numpy
+import theano
+import theano.tensor as T
+
+
+""" This is the libraries in activatefer functions. """
+
+percptron_type = [] # This array stored perceptron class name.
+
+def load(fname):
+    dat = numpy.load(fname)
+    if 'vbias_0' in dat:
+        vbias = dat['vbias_0']
+    else:
+        vbias = None
+    return generetor(dat['type_0'], dat['w_0'], dat['hbias_0'], dat['vbias_0'])
+
+def generetor(func, weight, hbias, vbias=None):
+    return eval(str(func)+'(weight,hbias,vbias)')
+
+class LinearLayer(object):
+
+    # Constructer
+    def __init__(self, weight, hbias, vbias):
+        floatX = theano.config.floatX
+
+        self.idim = weight.shape[0]
+        self.odim = weight.shape[1]
+
+        # Allocate the memory of parameter.
+        self.w    = theano.shared(value=weight)
+        self.bias = theano.shared(value=hbias)
+        if vbias is None:
+            self.vbias = theano.shared(
+                    value=numpy.zeros(self.odim, 
+                                      dtype=theano.config.floatX))
+        else:
+            self.vbias = theano.shared(value=vbias)
+
+    # Transfer function.
+    def forward(self, x):
+        return T.dot(x, self.w) + self.bias
+
+    def inverse(self):
+        return generetor(self.__class__.__name__, self.w.get_value().T, self.vbias.get_value(), self.bias.get_value())
+    
+    def save(self, fname, acttype="LinearLayer"):
+        numpy.savez(fname, type_0  = acttype, 
+                           w_0     = self.w.get_value(), 
+                           hbias_0 = self.bias.get_value(), 
+                           vbias_0 = self.bias.get_value()) 
+
+# Softmax class
+class SoftmaxLayer(LinearLayer):
+    def __init__(self, weight, bias, vbias):
+        super(SoftmaxLayer, self).__init__(weight, bias, vbias)
+    def forward(self, x):
+        return T.nnet.softmax(super(SoftmaxLayer, self).forward(x))
+    def save(self, fname):
+        super(SoftmaxLayer, self).save(fname, self.__class__.__name__)
+
+
+# Sigmoid class
+class SigmoidLayer(LinearLayer):
+    def __init__(self, weight, bias, vbias):
+        super(SigmoidLayer, self).__init__(weight, bias, vbias)
+    def forward(self, x):
+        return T.nnet.sigmoid(super(SigmoidLayer, self).forward(x))
+    def save(self, fname):
+        super(SigmoidLayer, self).save(fname, self.__class__.__name__)       
+               
+
+        
+
+# Rectified Linear class.
+class ReLULayer(LinearLayer):
+    def __init__(self, weight, bias,vbias):
+        super(ReLULayer, self).__init__(weight, bias, vbias)
+    def forward(self, x):
+        return T.max(super(ReLULayer, self).forward(x))
+    def save(self, fname):
+        super(ReLULayer, self).save(fname, self.__class__.__name__)       
+
+myself = sys.modules[__name__]
+percptron_type = [n for n,m in inspect.getmembers(myself, inspect.isclass)]
+
