Implement dataio.py And On the way to bug fix of backprop.py

backprop.py
@@ -10,83 +10,91 @@ from optparse import OptionParser
 import numpy as np
 import theano
 import theano.tensor as T
-from mylibs import perceptron
+from mylibs import *
 from mylibs import mlp
+from mylibs import dataio
 
-def shuffle(dat, lab):
+def shuffle(dat, tar):
     state = np.random.get_state()
     np.random.shuffle(dat)
     np.random.set_state(state)
-    np.random.shuffle(lab)
+    np.random.shuffle(tar)
     return
 
 if __name__=='__main__':
     floatX = theano.config.floatX
     # Option analysis.
-    usage="%prog [options] [data label]...\n
-            [data label] must be an even number.The Even-numbered is the input and the odd-numbered is the output correspondig to the input."
+    usage="""%prog [op] [data tarel]...\n
+            [data tarel] must be an even number.The Even-numbered is the input and the odd-numbered is the output correspondig to the input."""
     desc ="Training DNN with backpropagation."
     op = OptionParser(usage=usage, description=desc)
     
     # Required option.
-    op.add_option("-l", "--init-layer", action="append", dest="layers",
+    op.add_option("-l", "--init-layer", action="store", dest="layer",
             metavar="FILE", type="string", help="Initial value of MLP")
-    op.add_option("-o",  "--output", action="store", dest="output",metavar="FILE"
-            type="string",help="output file name which is npz format")
+    op.add_option("-o",  "--output", action="store", dest="output",
+            metavar="FILE", type="string",
+            help="output file name which is npz format")
     op.add_option("--lr", action="store", dest="lr",
             type="float", help="learning rate")
     op.add_option("--mm", action="store", dest="mm",
             type="float", help="momentum")
     op.add_option("--re", action="store", dest="re",
-            type="float", help="L2-reguralizer")
+           type="float", help="L2-reguralizer")
     op.add_option("-e",  "--epoch", action="store", dest="epoch",
             type="int",   help="Training epoch.")
     op.add_option("--mb", action="store", dest="mb",
             type="int",   help="The size of minibatch.")
+    op.add_option("--df", action="store", dest="df",
+            type="string", help="the format of training data.")
+    op.add_option("--tf", action="store", dest="tf",
+            type="string", help="the format of target data.")
     op.add_option("--ot", action="store", dest="ot",
-            type="choice", choices=["c", "f"], metavar="c/f",
-            help="output layer type is category or feature vector")
-    
+            type="choice", choices=['c','f'], metavar='f/c',
+            help="The target type is feature or category.")
+
     # Optional option.
     op.add_option("--seed", "--random-seed", action="store", dest="seed",
             type="int", default=1234,
             help="Random seed. [default=%default]")
 
     
-    (options, args) = op.parse_args()
-    mbsize = int(options.mb)
-    lr     = float(options.lr)
-    mm     = float(options.mm)
-    re     = float(options.re)
-    seed   = int(options.seed)
+    (op, args) = op.parse_args()
+    mbsize = int(op.mb)
+    lr     = float(op.lr)
+    mm     = float(op.mm)
+    re     = float(op.re)
+    seed   = int(op.seed)
 
 
     # load data.
-    m   = mlp.load(options.layer)
-    dat = np.loadtxt(FileInput(args[1::2]))
-    tar = np.loadtxt(Fileinput(args[2::2]))
+    m   = mlp.load(op.layer)
+    dat = dataio.load_data(args[0], op.df).reshape(-1, m[0].idim).astype(theano.config.floatX)
+    tar = dataio.load_data(args[1], op.tf).reshape(-1, m[-1].odim)
 
     # Allocate memory to update
     diffw    = []
     diffbias = []
     for i in m:
-        shape = i.w.shape
+        shape = i.w.get_value().shape
         diffw.append(
-                theano.shared(value=numpy.zeros(shape, dtype=floatX)).reshape(shape))
-        diffbais.append(
-                theano.shared(value=numpy.zeros(shape[1], dtype=floatX)))
+                theano.shared(value=np.zeros(shape[0]*shape[1], dtype=floatX).reshape(shape)))
+        diffbias.append(
+                theano.shared(value=np.zeros(shape[1], dtype=floatX)))
 
     # Formula to calcurate cost which is cross entropy.
-    if options.ot == 'c':
+    if op.ot == 'c':
+        tar  = np.asarray(tar, dtype=np.int32)
         y    = T.ivector("y")
-        cost = T.sum(T.nnet.categorical_crossentropy(mlp.f, y)) / mbsize
-    else if options.ot == 'f':
+        cost = T.sum(T.nnet.categorical_crossentropy(m.f, y)) / mbsize
+    elif op.ot == 'f':
+        tar  = tar.astype(np.float32)
         y    = T.fmatrix("y")
-        cost = T.sum((mlp.f-y)*(mlp.f-y)) / mbsize
+        cost = T.sum((m.f-y)*(m.f-y)) / mbsize
     
     # Formula to calcurate gradient.
     temp=[]
-    for layer in mlp:
+    for layer in m:
         temp.append(layer.w)
         temp.append(layer.bias)
     grads = T.grad(cost, temp)
@@ -94,41 +102,42 @@ if __name__=='__main__':
 
     # Formula to training.
     update_diff=[]
-    for i,layer in enumerate(L):
+    for i,layer in enumerate(m):
         update_diff.append((diffw[i],    -lr*(grads[i*2]   + re*layer.w  ) + mm*diffw[i]   ))
-        update_diff.append((diffbias[i], -lr*(grads[i*2+1] + re*layer.bais)+ mm*diffbias[i]))
+        update_diff.append((diffbias[i], -lr*(grads[i*2+1] + re*layer.bias)+ mm*diffbias[i]))
 
     update_update=[]
-    for i,layer in enumerate(L):
+    for i,layer in enumerate(m):
         update_update.append((layer.w,   layer.w    + diffw[i]   ))
         update_update.append((layer.bias,layer.bias + diffbias[i]))
-
-    trainer_diff  = theano.function(inputs=[x,y], outputs=cost, updates=updates_diff)
-    trainer_update= theano.function(intputs=[],   outputs=None, updates=update_update)
+    
+    x = m.x
+    trainer_diff  = theano.function(inputs=[x,y], outputs=cost, updates=update_diff)
+    trainer_update= theano.function(inputs=[],   outputs=None, updates=update_update)
 
     # Formula to eval.
-    if options.ot == 'c':
-        err=T.sum(T.neq(T.argmax(mlp.f, axis=1),y))
-        tester=theano.function(inputs=[x,y], output=err)
-    elif options.ot == 'f':
-        tester = theano.function(intput=[x,y], output=cost)
+    if op.ot == 'c':
+        if m[-1].odim == 1: err=T.sum(T.neq(T.ge(m.f, 0.5), y))
+        else              : err=T.sum(T.neq(T.argmax(m.f, axis=1),y))
+        tester=theano.function(inputs=[x,y], outputs=err)
+    elif op.ot == 'f':
+        tester = theano.function(intput=[x,y], outputs=cost)
 
     np.random.seed(seed)
-    mbnum = dat.shape()[1] / mbsize
-
+    mbnum = int(dat.shape[0] / mbsize)
     for i in range(seed):
-        shuffle(dat, lab)
+        shuffle(dat, tar)
 
         c=0.0
         e=0.0
-        for b in range(mbsize):
-            c += trainer_diff(dat[mbsize*b:(mbsize+1)*b], lab[mbsize*b:(mbsize+1)*b])
+        for b in range(mbnum):
+            c += trainer_diff(dat[mbsize*b:mbsize*(b+1)], tar[mbsize*b:mbsize*(b+1)])
             trainer_update()
-            e += tester(dat[mbsize*b:(mbsize+1)*b], lab[mbsize*b:(mbsize+1)*b])
+            e += tester(dat[mbsize*b:mbsize*(b+1)], tar[mbsize*b:mbsize*(b+1)])
         
         c/=mbnum*mbsize
         e/=mbnum*mbsize
 
         sys.stdout.write("%4d ephoch, cost= %0.8e mse= %0.8e\n" % (i, c, e))
-    mlp.save(options.output)
+    m.save(op.output)
 

mylibs/dataio.py
@@ -6,16 +6,25 @@
 import fileinput
 import numpy as np
 
-format_dict = {'f4be':'>f4', 'f4le':'<f4', 'f4ne':'=f4', 'npy':None, 'text':None}
+
+def __parse_fmt__(fmt):
+    r = ""
+    e = fmt[2:4]
+    if   e == "le": r = "<"
+    elif e == "be": r = ">"
+    elif e == "ne": r = "="
+    else:
+        raise NameError("The encoding %s is unknown." % fmt)
+    return r + fmt[0:2]
+    
+
 
 def load_data(fname, fmt):
-
-    if fmt not in format_dict.keys():
-        raise IOError('The format type %s is unknown.' %s fmt)
-
     if fmt == 'npy':
         return np.load(fname)
     elif fmt == 'text':
         return np.loadtxt(fname)
     else:
-        return np.fromfile(fname, dtype=format_dict[$fmt])
+        dt = np.dtype(__parse_fmt__(fmt))
+        return np.fromfile(fname, dtype=dt)
+